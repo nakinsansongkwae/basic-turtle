19a56fff1f6b54b3f2ce5e0fc53782824e771472
nakinsansongkwae <85365301+nakinsansongkwae@users.noreply.github.com>
2023-09-12 04:11:54 +0700

    Add files via upload

---
+++ tao.py
@@ -0,0 +1,15 @@
+import turtle
+
+tao = turtle.Pen()
+tao.shape('turtle')
+
+def taobin():
+    for i in range(4):
+        tao.fd(200)
+        tao.lt(90)
+
+taobin()        
+
+        
+
+        
